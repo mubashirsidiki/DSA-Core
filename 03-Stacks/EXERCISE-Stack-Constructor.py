aa7b932ae49eecc1338d29e489c4f3de43e9787c
mubashirsidiki <mubashirsidiki@gmail.com>
2024-05-27 17:28:14 +0500

    chore : Stack-Queue Class Added

---
+++ 03-Stacks/EXERCISE-Stack-Constructor.py
@@ -0,0 +1,33 @@
+# class Node:
+    ## WRITE NODE CONSTRUCTOR HERE ##
+    #                               #
+    #                               #
+    #                               #
+    #                               #
+    #################################
+        
+# class Stack:
+    ## WRITE STACK CONSTRUCTOR HERE ##
+    #                                #
+    #                                #
+    #                                #
+    #                                #
+    ##################################
+
+
+
+
+my_stack = Stack(4)
+
+print('Top:', my_stack.top.value)
+print('Height:', my_stack.height)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Top: 4
+    Height: 1
+
+"""
